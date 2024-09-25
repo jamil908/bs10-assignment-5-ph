feadf0f00f5e881c1b2496bf809b4e97117ce5b2
jamil9080 <md3711451@gmail.com>
2024-09-25 12:02:53 +0600

    fixed modal issue

--- js/comon-function.js
+++ js/comon-function.js
@@ -15,19 +15,17 @@ function getInnerTextById(id){
 
 function getAlert(addMoney,totalMoney){
     // addMoney= parseFloat(addMoney)
-    if(addMoney == 'number' ||  totalMoney > addMoney){
+    if(!isNaN(addMoney) && totalMoney>0 && totalMoney > addMoney){
         document.getElementById('my_modal_1').showModal();
        
         return true;
     }
-    if( isNaN(addMoney) ||  addMoney < 0 || totalMoney < addMoney){
-        document.getElementById('donate-money').removeAttribute('onclick')
-        document.getElementById('donate-money-two').removeAttribute('onclick')
-        document.getElementById('donate-money-third').removeAttribute('onclick')
+    if( isNaN(addMoney) || totalMoney<0 || addMoney < 0 || totalMoney < addMoney){
+        
         alert('You have failed to donate')
         document.getElementById('my_modal_1').close();
-        document.getElementById('my_modal_2').close();
-        document.getElementById('my_modal_3').close();
+        // document.getElementById('my_modal_2').close();
+        // document.getElementById('my_modal_3').close();
         return false
        
     }
